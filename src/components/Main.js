0e91a4fde102ef076cafcb62c6338b2c6b2d0d2f
Siyang Tao <siyangtao@Siyangs-MBP.gateway>
2017-12-10 11:54:37 +1000

    Add router.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom'
 import {Register} from './Register';
 import {Login} from './Login';
 
@@ -6,8 +7,13 @@ export class Main extends React.Component {
     render() {
         return (
             <div className="main">
-                {/*<Register/>*/}
-                <Login/>
+
+                <Switch>
+                    <Route exact path="/" component={Login}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/register" component={Register}/>
+                    <Route component={Login}/>
+                </Switch>
             </div>
         );
     }
