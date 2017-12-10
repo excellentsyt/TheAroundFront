6f67398487ae93279014ff31b111bbeecdf4a9ab
Siyang Tao <siyangtao@Siyangs-MBP.gateway>
2017-12-10 15:03:00 +1000

    New home page and re-direct add.

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,21 @@
-import React, { Component } from 'react';
-import { Header } from './Header';
-import { Main } from './Main';
+import React, {Component} from 'react';
+import {Header} from './Header';
+import {Main} from './Main';
 import '../styles/App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-          <Header/>
-          <Main/>
-      </div>
-    );
-  }
+    state = {
+        isLoggedIn: false
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <Header isLoggedIn={this.state.isLoggedIn}/>
+                <Main isLoggedIn={this.state.isLoggedIn}/>
+            </div>
+        );
+    }
 }
 
 export default App;
