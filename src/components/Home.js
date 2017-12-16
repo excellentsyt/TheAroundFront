de30e9a3c933d7f062fcb431e97f1b25b5d664b1
Siyang Tao <siyangtao@Siyangs-MBP.gateway>
2017-12-16 17:27:36 +1000

    Add posts ajax.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import { Tabs, Button, Spin } from 'antd';
 import { GEO_OPTIONS} from "../constants"
-import { POS_KEY } from "../constants"
+import { POS_KEY, AUTH_PREFIX, TOKEN_KEY, API_ROOT } from "../constants"
+import $ from 'jquery';
 
 const TabPane = Tabs.TabPane;
 const operations = <Button>Extra Action</Button>;
 
 export class Home extends React.Component {
     state = {
+        posts: [],
         error: "",
+        loadingPosts: false,
         loadingGeoLocation: false,
     }
 
@@ -37,25 +40,58 @@ export class Home extends React.Component {
             loadingGeoLocation: false,
         });
         const {latitude: lat, longitude: lon} = position.coords;
-        localStorage.setItem(POS_KEY, JSON.stringify({lat: lat, log: lon}));
+        localStorage.setItem(POS_KEY, JSON.stringify({lat: lat, lon: lon}));
+
+        this.loadNearByPosts();
     }
 
-    onFailedLoadGeoLocation = () => {
+    onFailedLoadGeoLocation = (error) => {
         this.setState({
             loadingGeoLocation: false,
             error: "Failed to load geo location!",
         });
     }
 
-    getGalleryPanelContent() {
+    getGalleryPanelContent = () => {
         if (this.state.error) {
             return <div>{this.state.error}</div>
         } else if (this.state.loadingGeoLocation) {
             // show spinner
             return <Spin tip="Loading geo location ..."/>
+        } else if (this.state.loadingPosts) {
+            return <Spin tip={"Loading posts ..."}/>
         }
 
         return null;
+    }
+
+    loadNearByPosts = () => {
+        //const { lat, lon } = JSON.parse(localStorage.getItem(POS_KEY));
+        const { lat, lon } = {"lat": 37.56, "lon": -122.3255};
+
+        this.setState({loadingPosts: true});
+        $.ajax({
+           url: `${API_ROOT}/search?lat=${lat}&lon=${lon}&range=20`,
+           method: 'GET',
+           headers: {
+               Authorization: `${AUTH_PREFIX} ${localStorage.getItem(TOKEN_KEY)}`,
+           }
+        }).then((response) => {
+                console.log(response);
+                this.setState({
+                    posts: response,
+                    loadingPosts: false,
+                });
+            }, (error) => {
+                this.setState({
+                    error: error.responseText,
+                    loadingPosts: false,
+                });
+            }
+        ).catch((error) => {
+                this.setState({ error: error });
+            }
+        );
     }
 
     render() {
