1dd8cb5787727290eb0b0cd7cb187eebfda691f1
Siyang Tao <siyangtao@Siyangs-MBP.gateway>
2017-12-16 15:48:33 +1000

    Get geolocstion.

--- src/components/Header.js
+++ src/components/Header.js
@@ -15,7 +15,7 @@ export class Header extends React.Component {
                 <img src={logo} className="App-logo" alt="logo" />
                 <h1 className="App-title">Around</h1>
                 {this.props.isLoggedIn ?
-                    <a href="" className="logout" onClick={this.props.handleLogout}>
+                    <a className="logout" onClick={this.props.handleLogout}>
                         <Icon type="logout" />{' '}logout
                     </a> : null
                 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,31 @@
 import React from 'react';
 import { Tabs, Button } from 'antd';
+import { GEO_OPTIONS} from "../constants"
+
 const TabPane = Tabs.TabPane;
 const operations = <Button>Extra Action</Button>;
 
 export class Home extends React.Component {
+    componentDidMount() {
+        if ("geolocation" in navigator) {
+            navigator.geolocation.getCurrentPosition(
+                this.onSuccessGeoLocation,
+                this.onFailedLoadGeoLocation,
+                GEO_OPTIONS,
+            );
+        } else {
+            console.log("geo location not supported!");
+        }
+    }
+
+    onSuccessGeoLocation = (position) => {
+        console.log(position);
+    }
+
+    onFailedLoadGeoLocation = () => {
+
+    }
+
     render() {
         return (
             <Tabs tabBarExtraContent={operations} className="main-tabs">
